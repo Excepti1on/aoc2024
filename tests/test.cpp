990c1450e044d010a778c957b570783f82c61853
Oliver Woelbing <oliver.woelbing@gmail.com>
2024-12-06 00:45:20 +0100

    updates

---
+++ tests/test.cpp
@@ -0,0 +1,40 @@
+//
+// Created by olive on 06/12/2024.
+//
+#include <iostream>
+#include <cassert>
+#include "../day1/day1.h"
+#include "../day2/day2.h"
+#include "../day3/day3.h"
+#include "../day4/day4.h"
+#include "../day5/day5.h"
+int main(int argc, char** argv) {
+  if(argc == 1){
+    return -1;
+  }
+  {
+    day1 day_1{argv[1]};
+    assert(day_1.part1() == 3246517);
+    assert(day_1.part2() == 493);
+  }
+  {
+    day2 day_2{argv[1]};
+    assert(day_2.part1() == 332);
+    assert(day_2.part2() == 398);
+  }
+  {
+    day3 day_3{argv[1]};
+    assert(day_3.part1() == 184511516);
+    assert(day_3.part2() == 90044227);
+  }
+  {
+    day4 day_4{argv[1]};
+    assert(day_4.part1() == 2507);
+    assert(day_4.part2() == 1969);
+  }
+  {
+    day5 day_5{argv[1]};
+    assert(day_5.part1() == 7307);
+    assert(day_5.part2() == 4713);
+  }
+}
