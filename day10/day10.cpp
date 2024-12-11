efe6be04d0906e9123759c522691ac2c9f8b2807
Oliver Woelbing <oliver.woelbing@gmail.com>
2024-12-11 12:37:25 +0100

    day10

--- day10/day10.cpp
+++ day10/day10.cpp
@@ -3,63 +3,109 @@
 #include <iostream>
 #include <queue>
 #include <vector>
-static auto read(std::ifstream& file) {
-    file.clear();
-    file.seekg(std::ios::beg);
-    std::vector<char> heightmap{};
-    std::size_t size = 0;
-    for (char c; file.get(c);) {
-        if (c >= '0' && c <= '9') {
-            heightmap.push_back(c);
-            size++;
-        } else {
-            size = 0;
-        }
+#include <unordered_set>
+#include <stack>
+#include <algorithm>
+static auto read(std::ifstream &file) {
+  file.clear();
+  file.seekg(std::ios::beg);
+  std::vector<char> heightmap{};
+  std::size_t size = 0;
+  std::size_t pos = 0;
+  for (char c; file.get(c);) {
+    if (c >= '0' && c <= '9') {
+      heightmap.push_back(c);
+      pos++;
+    } else {
+      size = pos;
+      pos = 0;
     }
-    return std::pair(heightmap, size);
+  }
+  return std::pair(heightmap, size);
 }
 
-static auto calculate_paths(std::size_t pos, const std::vector<char>& heightmap,
+static auto calculate_paths(std::size_t pos, const std::vector<char> &heightmap,
                             const std::size_t size) {
-    std::size_t paths = 0;
-    std::queue<std::size_t> Q{};
-    Q.push(pos);
-    while (!Q.empty()) {
-        auto v = Q.front();
-        Q.pop();
-        if (v == '9') {
-            paths++;
-        }
-        if (v + 1 < heightmap.size() - 1 && heightmap[v + 1] == heightmap[v] + 1) {
-            Q.push(v + 1);
-        }
-        if (v - 1 >= 0 && heightmap[v - 1] == heightmap[v] + 1) {
-            Q.push(v - 1);
-        }
-        if (v + size < heightmap.size() - 1 && heightmap[v + size] == heightmap[v] + 1) {
-            Q.push(v + size);
-        }
-        if (v >= size && heightmap[v - size] == heightmap[v] + 1) {
-            Q.push(v - size);
-        }
+  std::size_t paths = 0;
+  std::queue<std::size_t> Q{};
+  std::vector<char> visited(size * size, 0);
+  Q.push(pos);
+  visited[pos] = 1;
+  while (!Q.empty()) {
+    auto v = Q.front();
+    Q.pop();
+    if (heightmap[v] == '9') {
+      paths++;
     }
-    return paths;
+    if ((v % size != size - 1) && (heightmap[v + 1] == heightmap[v] + 1) && !visited[v + 1]) {
+      Q.push(v + 1);
+      visited[v + 1] = 1;
+    }
+    if ((v % size != 0) && (heightmap[v - 1] == heightmap[v] + 1) && !visited[v - 1]) {
+      Q.push(v - 1);
+      visited[v - 1] = 1;
+    }
+    if ((v + size < heightmap.size()) && (heightmap[v + size] == heightmap[v] + 1) && !visited[v + size]) {
+      Q.push(v + size);
+      visited[v + size] = 1;
+    }
+    if ((v >= size) && (heightmap[v - size] == heightmap[v] + 1) && !visited[v - size]) {
+      Q.push(v - size);
+      visited[v - size] = 1;
+    }
+  }
+  return paths;
+}
+static auto calculate_ratings(std::size_t pos, const std::vector<char> &heightmap,
+                              const std::size_t size) {
+  std::size_t paths = 0;
+  std::stack<std::size_t> Q{};
+  Q.push(pos);
+  while (!Q.empty()) {
+    auto v = Q.top();
+    Q.pop();
+    if (heightmap[v] == '9') {
+      paths++;
+    }
+    if ((v % size != size - 1) && (heightmap[v + 1] == heightmap[v] + 1)) {
+      Q.push(v + 1);
+    }
+    if ((v % size != 0) && (heightmap[v - 1] == heightmap[v] + 1)) {
+      Q.push(v - 1);
+    }
+    if ((v + size < heightmap.size()) && (heightmap[v + size] == heightmap[v] + 1)) {
+      Q.push(v + size);
+    }
+    if ((v >= size) && (heightmap[v - size] == heightmap[v] + 1)) {
+      Q.push(v - size);
+    }
+  }
+  return paths;
 }
 std::uint64_t day10::part1() {
-    auto [heightmap, size] = read(file);
-    std::vector<std::size_t> trailheads{};
-    for (std::size_t i = 0; i < heightmap.size(); ++i) {
-        if (heightmap[i] == '0') {
-            trailheads.push_back(i);
-        }
+  auto [heightmap, size] = read(file);
+  std::vector<std::size_t> trailheads{};
+  for (std::size_t i = 0; i < heightmap.size(); ++i) {
+    if (heightmap[i] == '0') {
+      trailheads.push_back(i);
     }
-    for (auto& head : trailheads) {
-        head = calculate_paths(head, heightmap, size);
-    }
-    for (const auto& head : trailheads) {
-        std::cout << head << std::endl;
-    }
-    return 0;
+  }
+  for (auto &head : trailheads) {
+    head = calculate_paths(head, heightmap, size);
+  }
+  return *std::ranges::fold_left_first(trailheads, std::plus{});
 }
 
-std::uint64_t day10::part2() { return 0; }
+std::uint64_t day10::part2() {
+  auto [heightmap, size] = read(file);
+  std::vector<std::size_t> trailheads{};
+  for (std::size_t i = 0; i < heightmap.size(); ++i) {
+    if (heightmap[i] == '0') {
+      trailheads.push_back(i);
+    }
+  }
+  for (auto &head : trailheads) {
+    head = calculate_ratings(head, heightmap, size);
+  }
+  return *std::ranges::fold_left_first(trailheads, std::plus{});
+}
